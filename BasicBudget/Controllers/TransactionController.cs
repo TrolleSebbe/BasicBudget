85bf6f824722e7e9d3f8f0dbb69ffce8eac6261b
Sebastian Alteryd <sebastian.alteryd@gmail.com>
2020-02-11 23:03:22 +0100

    inital files for transaction list

---
+++ BasicBudget/Controllers/TransactionController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BasicBudget.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionController
+    {
+        
+    }
+}
---
+++ BasicBudget/Transation.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BasicBudget
+{
+    public class Transation
+    {
+        public string Name { get; set; }
+
+        public int Amount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
