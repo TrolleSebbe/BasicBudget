9df265bf5a410d9b47dcc791a79f6d4937a7d6db
Trolle Geuna <trolle.geuna@gmail.com>
2020-02-14 00:20:53 +0100

    Fixed the loading problem..
    Error while  creating the path in the app module

--- BasicBudget/ClientApp/src/App.js
+++ BasicBudget/ClientApp/src/App.js
@@ -17,7 +17,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
-        <Route path='budgetlist' component={BudgetList} />
+        <Route path='/budget-list' component={BudgetList} />
       </Layout>
     );
   }
