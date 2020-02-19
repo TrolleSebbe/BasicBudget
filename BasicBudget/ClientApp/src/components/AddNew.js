fae0456a5cba068b5b67f783354f30f8431337e5
Sebastian Alteryd <sebastian.alteryd@gmail.com>
2020-02-19 22:53:20 +0100

    testing stuff

--- BasicBudget/ClientApp/src/components/AddNew.js
+++ BasicBudget/ClientApp/src/components/AddNew.js
@@ -6,10 +6,20 @@ export class AddNew extends Component {
     addNew(event) {
       console.log('I worked')
       event.preventDefault()
-      var xhr = new XMLHttpRequest()
+      const data = new FormData(event.target);
+      let jsonObject = {};
+      for (const [key, value]  of data.entries()) { jsonObject[key] = value; }
+      let json = JSON.stringify(jsonObject);
+      console.log(json);
+      fetch('/api/transactions/addtransaction', {
+        method: 'POST',
+        body: json,
+      });
+      /* var xhr = new XMLHttpRequest()
       xhr.open('POST', '/api/transactions/addtransaction')
-      xhr.send(JSON.stringify({ shortname: "test" }))
-      console.log(JSON.stringify({shortname: "test"}))
+      xhr.setRequestHeader("Content-Type", "application/json")
+      xhr.send(JSON.stringify({ shortName: "test" }))
+      console.log(JSON.stringify({shortName: "test"})) */
     }
 
     render() {
@@ -22,16 +32,16 @@ export class AddNew extends Component {
           <Form onSubmit={this.addNew}>
               <FormGroup>
                   <Label for="Name">Name</Label>
-                  <Input type="text" name="ShortName" id="Name" placeholder="Add a short description of the budgetpost" />
+                  <Input type="text" name="shortName" id="Name" placeholder="Add a short description of the budgetpost" />
               </FormGroup>
               <FormGroup>
                   <Label for="Description">Description</Label>
                   <Input type="textarea" name="description" id="Description" placeholder="Add a description of the budgetpost" />
               </FormGroup>
-              <FormGroup>
+{/*               <FormGroup>
                   <Label for="Amount">Amount</Label>
-                  <Input type="number" name="Amount" id="Amount" placeholder="Add a Amount for budgetPost" />
-              </FormGroup>
+                  <Input type="number" name="amount" id="Amount" placeholder="Add a Amount for budgetPost" />
+              </FormGroup> */}
               <Button color="primary" id="submit" type="submit">Submit new post</Button>
           </Form>
           
