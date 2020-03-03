4ba8bfb0964bd73aba69882faebeeffc842d791e
Sebastian Alteryd <sebastian.alteryd@gmail.com>
2020-03-03 19:39:37 +0100

    beautify some code

--- BasicBudget/ClientApp/src/components/AddNew.js
+++ BasicBudget/ClientApp/src/components/AddNew.js
@@ -1,57 +1,77 @@
-import React, { Component } from 'react';
-import { Button, UncontrolledCollapse, Card, CardBody, Form, FormGroup, Label, Input } from 'reactstrap';
+import React, {Component} from 'react';
+import {
+    Button,
+    UncontrolledCollapse,
+    Card,
+    CardBody,
+    Form,
+    FormGroup,
+    Label,
+    Input
+} from 'reactstrap';
 export class AddNew extends Component {
 
-
     addNew(event) {
-      console.log('I worked')
-      event.preventDefault()
-      //Get formdata from form
-      const data = new FormData(event.target);
-      //Generate json object
-      let jsonObject = {};
-      jsonObject["shortName"] = data.get("shortName");
-      jsonObject["description"] = data.get("description");
-      jsonObject["amount"] = parseInt(data.get("amount"), 10);
-      //Do POST request
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', '/api/transactions/addtransaction');
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send(JSON.stringify(jsonObject));
-      console.log(JSON.stringify(jsonObject));
+        console.log('I worked')
+        event.preventDefault()
+        //Get formdata from form
+        const data = new FormData(event.target);
+        //Generate json object
+        let jsonObject = {};
+        jsonObject["shortName"] = data.get("shortName");
+        jsonObject["description"] = data.get("description");
+        jsonObject["amount"] = parseInt(data.get("amount"), 10);
+        //Do POST request
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/api/transactions/addtransaction');
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(jsonObject));
+        console.log(JSON.stringify(jsonObject));
     }
 
     render() {
-    return (
-      <div>
-           <Button color="primary" id="toggler" style={{ marginBottom: '1rem' }}>Add New Bugetpost</Button>
-    <UncontrolledCollapse toggler="#toggler">
-      <Card>
-        <CardBody>
-          <Form onSubmit={this.addNew}>
-              <FormGroup>
-                  <Label for="Name">Name</Label>
-                  <Input type="text" name="shortName" id="Name" placeholder="Add a short description of the budgetpost" />
-              </FormGroup>
-              <FormGroup>
-                  <Label for="Description">Description</Label>
-                  <Input type="textarea" name="description" id="Description" placeholder="Add a description of the budgetpost" />
-              </FormGroup>
-              <FormGroup>
-                  <Label for="Amount">Amount</Label>
-                  <Input type="number" name="amount" id="Amount" placeholder="Add a Amount for budgetPost" />
-              </FormGroup>
-              <Button color="primary" id="submit" type="submit">Submit new post</Button>
-          </Form>
-          
-        </CardBody>
-      </Card>
-    </UncontrolledCollapse>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <Button
+                    color="primary"
+                    id="toggler"
+                    style={{
+                    marginBottom: '1rem'
+                }}>Add New Bugetpost</Button>
+                <UncontrolledCollapse toggler="#toggler">
+                    <Card>
+                        <CardBody>
+                            <Form onSubmit={this.addNew}>
+                                <FormGroup>
+                                    <Label for="Name">Name</Label>
+                                    <Input
+                                        type="text"
+                                        name="shortName"
+                                        id="Name"
+                                        placeholder="Add a short description of the budgetpost"/>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="Description">Description</Label>
+                                    <Input
+                                        type="textarea"
+                                        name="description"
+                                        id="Description"
+                                        placeholder="Add a description of the budgetpost"/>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="Amount">Amount</Label>
+                                    <Input
+                                        type="number"
+                                        name="amount"
+                                        id="Amount"
+                                        placeholder="Add a Amount for budgetPost"/>
+                                </FormGroup>
+                                <Button color="primary" id="submit" type="submit">Submit new post</Button>
+                            </Form>
+                        </CardBody>
+                    </Card>
+                </UncontrolledCollapse>
+            </div>
+        );
+    }
 }
-// Date = DateTime.Now.AddDays(index),
-//     ShortName = "TrolleBolle",
-//     Description = "Trolle testar stenhårt",
-//     Amount = randomizationVariable.Next(-1000, 1000)
--- BasicBudget/ClientApp/src/components/TransactionsList.js
+++ BasicBudget/ClientApp/src/components/TransactionsList.js
@@ -1,65 +1,69 @@
-import React, { Component } from 'react';
-import { AddNew } from './AddNew';
+import React, {Component} from 'react';
+import {AddNew} from './AddNew';
 
 export class TransactionsList extends Component {
-  static displayName = TransactionsList.name;
+    static displayName = TransactionsList.name;
 
-  constructor(props) {
-    super(props);
-    this.state = { transactions: [], loading: true, shortname: '' };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            transactions: [],
+            loading: true,
+            shortname: ''
+        };
+    }
 
-  componentDidMount() {
-    this.populateTransactionsList();
-  }
+    componentDidMount() {
+        this.populateTransactionsList();
+    }
 
-  handleChange = event => {
-    this.setState({ shortname: event.target.value });
-  }
+    handleChange = event => {
+        this.setState({shortname: event.target.value});
+    }
 
-  static renderTransactionsListTable(transactions) {
-    return (
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Short Description</th>
-            <th>Amount</th>
-            <th>Description</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.map(transactionsList =>
-            <tr key={transactionsList.id}>
-              <td>{transactionsList.shortName}</td>
-              <td>{transactionsList.amount}</td>
-              <td>{transactionsList.description}</td>
-              <td>{transactionsList.transactionDate}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
-  }
+    static renderTransactionsListTable(transactions) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Short Description</th>
+                        <th>Amount</th>
+                        <th>Description</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {transactions.map(transactionsList => <tr key={transactionsList.id}>
+                        <td>{transactionsList.shortName}</td>
+                        <td>{transactionsList.amount}</td>
+                        <td>{transactionsList.description}</td>
+                        <td>{transactionsList.transactionDate}</td>
+                    </tr>)}
+                </tbody>
+            </table>
+        );
+    }
 
-  render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : TransactionsList.renderTransactionsListTable(this.state.transactions);
+    render() {
+        let contents = this.state.loading
+            ? <p>
+                    <em>Loading...</em>
+                </p>
+            : TransactionsList.renderTransactionsListTable(this.state.transactions);
 
-    return (
-      <div>
-        <h1 id="tabelLabel" >Transactions</h1>
-        <p>Fetching from MongoDb</p>
-        <AddNew></AddNew>
-        {contents}
-      </div>
-    );
-  }
+        return (
+            <div>
+                <h1 id="tabelLabel">Transactions</h1>
+                <p>Fetching from MongoDb</p>
+                <AddNew></AddNew>
+                {contents}
+            </div>
+        );
+    }
 
-  async populateTransactionsList() {
-    const response = await fetch('api/Transactions/Get');
-    const data = await response.json();
-    this.setState({ transactions: data, loading: false });
-  }
+    async populateTransactionsList() {
+        const response = await fetch('api/Transactions/Get');
+        const data = await response.json();
+        this.setState({transactions: data, loading: false});
+    }
 }
